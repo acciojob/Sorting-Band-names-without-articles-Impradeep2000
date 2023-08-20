e7d5705eb62eedd2d381be9401a11017ed57eb96
Pradeep Kumar <95734296+Impradeep2000@users.noreply.github.com>
2023-08-20 20:43:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-//your code here
-let bandNames = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-
-function removeArticles(name) {
-    return name.replace(/^(a |an |the )/i, '');
+let bandNames=['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal']
+let articles = ['A', 'AN', 'THE']
+ 
+	bandNames.sort((a,b) => (strip(a)> strip(b))? 1: -1)
+	const ref= document.getElementsByTagName("ul")[0] 
+	for (let i=0;i<bandNames.length; i++){
+		const li = document.createElement("li")
+		li.innerText= bandNames [i]
+		ref.append(li)
+	}
+function strip(word){
+	let arr = word.split(" ")
+	let s=""
+	for(let i=0;i<arr.length;i++){
+if(articles.indexOf(arr[i].toUpperCase()) === -1){
+	s=s+arr[i]
 }
-
-bandNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
-
-const bandList = document.getElementById('band');
-for (const bandName of bandNames) {
-    const li = document.createElement('li');
-    li.textContent = bandName;
-    bandList.appendChild(li);
+}
+return s.trim()
 }
